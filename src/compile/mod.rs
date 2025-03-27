7cffe2385787636dff88517d8605fab005db24fa
Dmitry Poroh <dporoh@pm.me>
2025-03-27 14:51:39 -0700

    compile: header, cookie parameters resolution

--- src/compile/mod.rs
+++ src/compile/mod.rs
@@ -31,6 +31,8 @@ pub struct Operation<'a> {
     pub path: &'a Path,
     pub path_params: HashMap<&'a parameter::Name, &'a Parameter>,
     pub query_params: Vec<&'a Parameter>,
+    pub header_params: Vec<&'a Parameter>,
+    pub cookie_params: Vec<&'a Parameter>,
 }
 
 #[derive(Debug)]
@@ -41,17 +43,27 @@ pub enum Error<'a> {
     WrongParameterReference(&'a Path, &'a Reference),
 }
 
-pub fn compile(d: &schema::Description) -> Result<Compiled, Error> {
+type CResult<'a, T> = Result<T, Error<'a>>;
+
+pub fn compile(d: &schema::Description) -> CResult<Compiled> {
     let operations = d
         .paths
         .as_ref()
         .map(|paths| {
             Ok(paths
                 .iter()
-                .map(|(path, item)| -> Result<Vec<Operation>, Error> {
+                .map(|(path, item)| -> CResult<Vec<Operation>> {
                     item.operations_iter()
                         .map(|(op_type, op)| {
-                            compile_operation(path, item, op_type, op, &d.components)
+                            compile_operation(
+                                op_type,
+                                &OpCompileData {
+                                    path,
+                                    item,
+                                    op,
+                                    components: &d.components,
+                                },
+                            )
                         })
                         .collect::<Result<Vec<_>, _>>()
                 })
@@ -64,100 +76,46 @@ pub fn compile(d: &schema::Description) -> Result<Compiled, Error> {
     Ok(Compiled { operations })
 }
 
-pub fn compile_operation<'a>(
-    path: &'a schema::path::Path,
-    item: &'a PathItem,
+fn compile_operation<'a>(
     op_type: OperationType,
-    op: &'a schema::operation::Operation,
-    components: &'a Option<Components>,
+    cdata: &OpCompileData<'a>,
 ) -> Result<Operation<'a>, Error<'a>> {
-    let path_params = path
+    let path_params = cdata
+        .path
         .path_params_iter()
         .map(|pname| {
             let name = pname
                 .map_err(Error::PathParseError)
                 .map(|v| parameter::Name::new(v.into()))?;
-            resolve_parameter(op, &name, item, components)
-                .ok_or(Error::ParameterNotDefined(path, name.clone()))
+            resolve_path_parameter(&name, cdata)
+                .ok_or(Error::ParameterNotDefined(cdata.path, name.clone()))
                 .and_then(|p| match p.place {
                     parameter::Place::Path(_) => Ok(p),
-                    _ => Err(Error::NotDefinedAsPathParameter(path, name)),
+                    _ => Err(Error::NotDefinedAsPathParameter(cdata.path, name)),
                 })
                 .map(|v| (&v.name, v))
         })
         .collect::<Result<HashMap<_, _>, _>>()?;
 
-    let resolve_param = |p: &'a ParameterOrReference| match p {
-        ParameterOrReference::Parameter(p) => Ok(p),
-        ParameterOrReference::Reference(sref) => sref
-            .sref
-            .parameter_sref()
-            .and_then(|sref| components.as_ref().and_then(|c| c.find_parameter(&sref)))
-            .ok_or(Error::WrongParameterReference(path, sref)),
-    };
-    let filter_err_or_query = |v: &Result<&'a Parameter, _>| {
-        matches!(
-            v,
-            Ok(Parameter {
-                place: parameter::Place::Query(_),
-                ..
-            }) | Err(_)
-        )
-    };
-    let op_query_params = op
-        .parameters
-        .as_ref()
-        .map(|vec| {
-            vec.iter()
-                .map(resolve_param)
-                .filter(filter_err_or_query)
-                .collect::<Result<Vec<_>, _>>()
-        })
-        .unwrap_or(Ok(vec![]))?;
-
-    // Append all parameters from path that are not overriden by operation.
-    let all_query_names = op_query_params
-        .iter()
-        .map(|p| &p.name)
-        .collect::<HashSet<_>>();
-    let path_query_params = item
-        .parameters
-        .as_ref()
-        .map(|vec| {
-            vec.iter()
-                .map(resolve_param)
-                .filter(filter_err_or_query)
-                .filter(|v: &Result<&'a Parameter, _>| match v {
-                    Err(_) => true,
-                    Ok(Parameter { ref name, .. }) => !all_query_names.contains(&name),
-                })
-                .collect::<Result<Vec<_>, _>>()
-        })
-        .unwrap_or(Ok(vec![]))?;
-
     Ok(Operation {
         op_type,
-        path,
+        path: cdata.path,
         path_params,
-        query_params: [op_query_params, path_query_params].concat(),
+        query_params: compile_params_by_group(cdata, is_query_param)?,
+        header_params: compile_params_by_group(cdata, is_header_param)?,
+        cookie_params: compile_params_by_group(cdata, is_cookie_param)?,
     })
 }
 
-pub fn resolve_parameter<'a>(
-    op: &'a schema::operation::Operation,
+fn resolve_path_parameter<'a>(
     pname: &parameter::Name,
-    item: &'a PathItem,
-    components: &'a Option<Components>,
+    cdata: &OpCompileData<'a>,
 ) -> Option<&'a Parameter> {
     let find_param = |ps: &'a Vec<ParameterOrReference>| {
         for p in ps.iter() {
             let candidate = match p {
                 ParameterOrReference::Parameter(p) => Some(p),
-                ParameterOrReference::Reference(sref) => sref
-                    .sref
-                    .parameter_sref()
-                    .as_ref()
-                    .and_then(|sref| components.as_ref().and_then(|c| c.find_parameter(sref))),
+                ParameterOrReference::Reference(r) => cdata.find_param_by_ref(r),
             };
             if let Some(candidate) = candidate {
                 if &candidate.name == pname {
@@ -168,8 +126,106 @@ pub fn resolve_parameter<'a>(
         None
     };
     // Find parameter inside operation and after for whole path
-    op.parameters
+    cdata
+        .op
+        .parameters
         .as_ref()
         .and_then(find_param)
-        .or_else(|| item.parameters.as_ref().and_then(find_param))
+        .or_else(|| cdata.item.parameters.as_ref().and_then(find_param))
+}
+
+struct OpCompileData<'a> {
+    path: &'a Path,
+    item: &'a PathItem,
+    op: &'a schema::operation::Operation,
+    components: &'a Option<Components>,
+}
+
+impl<'a> OpCompileData<'a> {
+    fn find_param_by_ref(&self, r: &Reference) -> Option<&'a Parameter> {
+        r.sref.parameter_sref().as_ref().and_then(|sref| {
+            self.components
+                .as_ref()
+                .and_then(|c| c.find_parameter(sref))
+        })
+    }
+}
+
+fn is_query_param(p: &Parameter) -> bool {
+    matches!(
+        p,
+        Parameter {
+            place: parameter::Place::Query(_),
+            ..
+        }
+    )
+}
+
+fn is_header_param(p: &Parameter) -> bool {
+    matches!(
+        p,
+        Parameter {
+            place: parameter::Place::Header(_),
+            ..
+        }
+    )
+}
+
+fn is_cookie_param(p: &Parameter) -> bool {
+    matches!(
+        p,
+        Parameter {
+            place: parameter::Place::Cookie(_),
+            ..
+        }
+    )
+}
+
+type FilterFunc = fn(&Parameter) -> bool;
+
+fn compile_params_by_group<'a>(
+    cdata: &OpCompileData<'a>,
+    filter: FilterFunc,
+) -> Result<Vec<&'a Parameter>, Error<'a>> {
+    let resolve_param = |p: &'a ParameterOrReference| match p {
+        ParameterOrReference::Parameter(p) => Ok(p),
+        ParameterOrReference::Reference(r) => cdata
+            .find_param_by_ref(r)
+            .ok_or(Error::WrongParameterReference(cdata.path, r)),
+    };
+    let filter_or_err = |v: &Result<&'a Parameter, _>| match v {
+        Ok(p) => filter(p),
+        Err(_) => true,
+    };
+    let op_params = cdata
+        .op
+        .parameters
+        .as_ref()
+        .map(|vec| {
+            vec.iter()
+                .map(resolve_param)
+                .filter(filter_or_err)
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .unwrap_or(Ok(vec![]))?;
+
+    // Append all parameters from path that are not overriden by operation.
+    let all_names = op_params.iter().map(|p| &p.name).collect::<HashSet<_>>();
+    let path_params = cdata
+        .item
+        .parameters
+        .as_ref()
+        .map(|vec| {
+            vec.iter()
+                .map(resolve_param)
+                .filter(filter_or_err)
+                .filter(|v: &Result<&'a Parameter, _>| match v {
+                    Err(_) => true,
+                    Ok(Parameter { ref name, .. }) => !all_names.contains(&name),
+                })
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .unwrap_or(Ok(vec![]))?;
+
+    Ok([op_params, path_params].concat())
 }
